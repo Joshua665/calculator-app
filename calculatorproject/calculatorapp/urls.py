219b4171f66e4133061e4c27bb9ad38667a281bb
Joshua West <west7828@run.edu.ng>
2021-08-27 01:57:16 -1200

    more updates

--- calculatorproject/calculatorapp/urls.py
+++ calculatorproject/calculatorapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('calculatorapp', views.calculatorapp, name = 'calculatorapp'),
+    path('submitquery', views.submitquery, name='submitquery'),
 ]
